d6ae9edfe8b8aab10c6d70e8dbc90308e54cdb32
B Kleinen <kleinen@infrastructure.de>
2018-05-04 05:45:58 +0200

    added getClockDisplay and update button to GUI to ease testing, size adjusts to actual displayString at startup

--- clock-display-with-GUI/Clock.java
+++ clock-display-with-GUI/Clock.java
@@ -18,16 +18,16 @@ public class Clock
     private ClockDisplay clock;
     private boolean clockRunning = false;
     private TimerThread timerThread;
-    
+
     /**
      * Constructor for objects of class Clock
      */
     public Clock()
     {
-        makeFrame();
         clock = new ClockDisplay();
+        makeFrame();
     }
-    
+
     /**
      * 
      */
@@ -37,7 +37,7 @@ public class Clock
         timerThread = new TimerThread();
         timerThread.start();
     }
-    
+
     /**
      * 
      */
@@ -49,24 +49,32 @@ public class Clock
     /**
      * 
      */
+    private void update()
+    {
+        label.setText(clock.getTime());
+    }
+    
+    /**
+     * 
+     */
     private void step()
     {
         clock.timeTick();
         label.setText(clock.getTime());
     }
-    
+
     /**
      * 'About' function: show the 'about' box.
      */
     private void showAbout()
     {
         JOptionPane.showMessageDialog (frame, 
-                    "Clock Version 1.0\n" +
-                    "A simple interface for the 'Objects First' clock display project",
-                    "About Clock", 
-                    JOptionPane.INFORMATION_MESSAGE);
+            "Clock Version 1.0\n" +
+            "A simple interface for the 'Objects First' clock display project",
+            "About Clock", 
+            JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
     /**
      * Quit function: quit the application.
      */
@@ -75,7 +83,6 @@ public class Clock
         System.exit(0);
     }
 
-    
     /**
      * Create the Swing frame and its content.
      */
@@ -86,12 +93,12 @@ public class Clock
         contentPane.setBorder(new EmptyBorder(1, 60, 1, 60));
 
         makeMenuBar(frame);
-        
+
         // Specify the layout manager with nice spacing
         contentPane.setLayout(new BorderLayout(12, 12));
-        
+
         // Create the image pane in the center
-        label = new JLabel("00:00", SwingConstants.CENTER);
+        label = new JLabel(clock.getTime(), SwingConstants.CENTER);
         Font displayFont = label.getFont().deriveFont(96.0f);
         label.setFont(displayFont);
         //imagePanel.setBorder(new EtchedBorder());
@@ -100,14 +107,18 @@ public class Clock
         // Create the toolbar with the buttons
         JPanel toolbar = new JPanel();
         toolbar.setLayout(new GridLayout(1, 0));
-        
+
         JButton startButton = new JButton("Start");
         startButton.addActionListener(e -> start());
         toolbar.add(startButton);
-        
+
         JButton stopButton = new JButton("Stop");
         stopButton.addActionListener(e -> stop());
         toolbar.add(stopButton);
+
+        JButton updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> update());
+        toolbar.add(updateButton);
 
         JButton stepButton = new JButton("Step");
         stepButton.addActionListener(e -> step());
@@ -116,18 +127,18 @@ public class Clock
         // Add toolbar into panel with flow layout for spacing
         JPanel flow = new JPanel();
         flow.add(toolbar);
-        
+
         contentPane.add(flow, BorderLayout.SOUTH);
-        
+
         // building is done - arrange the components      
         frame.pack();
-        
+
         // place the frame at the center of the screen and show
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
         frame.setVisible(true);
     }
-    
+
     /**
      * Create the main frame's menu bar.
      * 
@@ -140,26 +151,26 @@ public class Clock
 
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
-        
+
         JMenu menu;
         JMenuItem item;
-        
+
         // create the File menu
         menu = new JMenu("File");
         menubar.add(menu);
-        
+
         item = new JMenuItem("About Clock...");
-            item.addActionListener(e -> showAbout());
+        item.addActionListener(e -> showAbout());
         menu.add(item);
 
         menu.addSeparator();
-        
+
         item = new JMenuItem("Quit");
-            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
-            item.addActionListener(e -> quit());
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
+        item.addActionListener(e -> quit());
         menu.add(item);
     }
-    
+
     class TimerThread extends Thread
     {
         public void run()
@@ -169,7 +180,7 @@ public class Clock
                 pause();
             }
         }
-        
+
         private void pause()
         {
             try {
@@ -178,5 +189,8 @@ public class Clock
             catch (InterruptedException exc) {
             }
         }
+    }
+    public ClockDisplay getClockDisplay(){
+        return clock;
     }
 }
